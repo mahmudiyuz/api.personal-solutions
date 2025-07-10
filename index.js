79ea4a492167ee7c25955d97ce3fa855cba7f80a
Muhammadqodir_Mahmudiy <muhammadqodirhasanov@gmail.com>
2025-07-10 14:26:23 +0500

    finished

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-const express = require("express");
-const { Pool } = require("pg");
 require("dotenv").config();
-
+const express = require("express");
+const db = require("./db");
 const app = express();
-const port = process.env.PORT || 3000;
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
 
 app.get("/vacancies", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM vacancies");
-    res.json(result.rows);
+    const { rows } = await db.query("SELECT * FROM vacancies");
+    res.json(rows);
   } catch (err) {
-    console.error(err);
     res.status(500).json({ error: err });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server started`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
