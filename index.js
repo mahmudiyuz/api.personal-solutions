45d51565c82297800a7d453dca18801a8b9df6ee
Muhammadqodir_Mahmudiy <muhammadqodirhasanov@gmail.com>
2025-07-11 10:54:50 +0500

    update api and some optimizations

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Pool } = require("pg");
 
 const app = express();
 app.use(express.json());
+
 require("dotenv").config();
 
 const pool = new Pool();
@@ -46,27 +47,78 @@ app.post("/create", async (req, res) => {
   }
 });
 
+// update
+app.put("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name, salary, adress, conditions, responsibility, requirements } =
+    req.body;
+  const values = [
+    id,
+    name,
+    salary,
+    adress,
+    conditions,
+    responsibility,
+    requirements,
+  ];
+
+  try {
+    const result = await pool.query(
+      `
+        UPDATE vacancies
+        SET name = $2,
+            salary = $3,
+            adress = $4,
+            conditions = $5,
+            responsibility = $6,
+            requirements = $7
+        WHERE id = $1
+          AND state = 1
+        RETURNING *;
+      `,
+      values
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "Data not found or inactive" });
+    }
+
+    res.status(200).json({ message: "Data is updated", data: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+});
+
 // delete
 app.delete("/delete/:id", async (req, res) => {
   const id = req.params.id;
   const values = [id];
 
-  const result = await pool.query(
-    `
-      UPDATE vacancies
-      SET state = 0
-      WHERE id = $1
-      RETURNING *;
-    `,
-    values
-  );
+  try {
+    const result = await pool.query(
+      `
+        UPDATE vacancies
+        SET state = 0
+        WHERE id = $1
+        RETURNING *;
+      `,
+      values
+    );
 
-  if (result.rowCount === 0) {
-    return res.status(404).json({ message: "Data not found." });
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "Not found." });
+    }
+
+    res.status(200).json({ message: "Data is deleted.", data: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
   }
-
-  res.status(200).json({ message: "Data is deleted.", data: result.rows[0] });
 });
 
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+// listen
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
